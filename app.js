999d5b56a65ce0420e2e28bdd5e62a045f5f3c55
Tian Xiang Tee <kyoxiang99@gmail.com>
2021-10-16 20:44:52 +0800

    Init env variable

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
 const characterRouter = require("./routes/characterRoutes");
@@ -12,10 +13,14 @@ app.use(express.json());
 // To allow access to static file
 app.use(express.static(`${__dirname}/public`));
 
-app.use((req, res, next) => {
-  console.log("I am middleware");
-  next();
-});
+// Optional log package
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+  app.use((req, res, next) => {
+    console.log("I am custom middleware");
+    next();
+  });
+}
 
 //2. Setup Routes
 app.use("/api/v1/characters", characterRouter);
--- server.js
+++ server.js
@@ -1,7 +1,13 @@
+const dotenv = require("dotenv");
+
+// IMPORTANT : SETUP CONFIG ENV before app declare
+dotenv.config({ path: "./config.env" });
 const app = require("./app");
 
+// Check env
+// console.log(process.env);
 //Get express app from app.js to setup server
-const port = 3000;
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
-  console.log("Starting server ...");
+  console.log(`Starting ${process.env.NODE_ENV} server`);
 });
