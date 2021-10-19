a2a7d41618f297739450c61495cd9ee8bc03c174
Tian Xiang Tee <kyoxiang99@gmail.com>
2021-10-19 10:34:54 +0800

    Setup mongo connection

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 // IMPORTANT : SETUP CONFIG ENV before app declare
 dotenv.config({ path: "./config.env" });
 const app = require("./app");
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    // useNewUrlParser: true,
+    // useCreateIndex: true,
+    // useFindAndModify: false,
+  })
+  .then(() => console.log("DB connection successful!"));
 
 // Check env
 // console.log(process.env);
